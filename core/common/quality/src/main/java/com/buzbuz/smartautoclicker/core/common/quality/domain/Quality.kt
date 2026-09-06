b6a32d7f337c780cc055c669bb6b09d629fc1cf2
Nain57 <buzbuz.software@gmail.com>
2026-09-06 12:36:56 +0200

    Fix debugging values for quality metrics
    This only impact playstore metrics values so no need to release an hotfix,
    as it is not realeased yet.
    Playstore 4.0.2 will point on this version

--- core/common/quality/src/main/java/com/buzbuz/smartautoclicker/core/common/quality/domain/Quality.kt
+++ core/common/quality/src/main/java/com/buzbuz/smartautoclicker/core/common/quality/domain/Quality.kt
@@ -17,6 +17,8 @@
 package com.buzbuz.smartautoclicker.core.common.quality.domain
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 /** Describe the different quality levels felt by the user. */
@@ -29,11 +31,11 @@ sealed class Quality(internal val backToHighDelay: Duration? = null) {
     data object High : Quality()
 
     /** The issue has occurred due to external perturbations, such as aggressive background service management */
-    data object ExternalIssue : Quality(1.seconds)
+    data object ExternalIssue : Quality(5.minutes)
 
     /** The issue has occurred due to a crash of Smart AutoClicker. */
-    data object Crashed : Quality(1.seconds)
+    data object Crashed : Quality(1.hours)
 
     /** The user is using the app for the first time. */
-    data object FirstTime : Quality(1.seconds)
+    data object FirstTime : Quality(30.minutes)
 }
